1fd6cd659288337cfe3773862a20bfe90ce33ef9
Glitch (glitch-hello-website) <none>
2022-11-03 21:58:24 +0000

    🛫🚐 Checkpoint ./app.js:270926/588

--- app.js
+++ app.js
@@ -13,15 +13,33 @@ window.addEventListener("load", function () {
 	// VUE!!!
 	// Create a new vue interface
 
-  console.log("LOADING DATA....")
-  d3.csv("data/perfume_data.csv", function(data) {
-    console.log("loaded data???")
-    for (var i = 0; i < data.length; i++) {
-       console.log(data[i])
-    }
-});
+//   console.log("LOADING DATA....")
+//   d3.csv("data/perfume_data.csv", function(data) {
+//     console.log("loaded data???")
+//     for (var i = 0; i < data.length; i++) {
+//        console.log(data[i])
+//     }
+// });
   
+  let url = 'data/hail_size.json'
+  // console.log("REQUEST")
+  // fetch('data/hail_size.json')
+  //   .then((response) => {
+  //   console.log(response)
+  //   response.json()
+  // })
+  //   .then((json) => console.log(json));
   
+  fetch(urlToUser, parameters)
+  .then(response => {
+    return response.text()
+  })
+  .then((data) => {
+    resolve(data ? JSON.parse(data) : {})
+  })
+  .catch((error) => {
+    reject(error)
+  })
 	new Vue({
 		template: `<div id="app">
 			
