54d8c85fe35355ca6a9dd9c542eda0f87c28aabb
Glitch (glitch-hello-website) <none>
2022-11-07 02:25:42 +0000

    🚆🚂 Checkpoint ./app.js:270926/7006 ./utilities.js:270926/1909 ./index.html:270926/61

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 import xkcdData from "./xkcd.json" assert { type: "json" };
-console.log(xkcdData)/**
+import hexToHSL from "./utilities.js"
+console.log(xkcdData)
+
+// Preprocess data
+class Color {
+  constructor({hex, name}) {
+    this.hex = hex
+    this.name = name
+    this.hsl = hexToHSL(hex)
+  }
+}
+
+let colors = xkcdData.colors.map()
+
+/**
 
  * Starter code
- * Chat application with a bot
+ * Data visualization
  */
 
-/* globals Vue, p5, BOT_MAPS, BotSimulator */
+/* globals Vue, p5*/
 
 window.addEventListener("load", function () {
 	//------------------------------------------------------
@@ -16,14 +30,50 @@ window.addEventListener("load", function () {
 	// Create a new vue interface
 
 
-
+   let p = undefined
+  const CANVAS_WIDTH = 400
+  const CANVAS_HEIGHT = 200
+  
 	new Vue({
 		template: `<div id="app">
-			
-		  APP
-      <div v-for="color in xkcdData.colors">{{color}}</div>
-		</div>`,
+	    <div ref="canvasHolder"></div>		
+		  
+      <div v-for="color in xkcdData.colors" :style="{backgroundColor:color.hex }">{{color}}</div>
+	
+  </div>`,
+     
    
+    mounted() {
+      // Create P5
+       const s = (p0) => {
+        p = p0;
+
+        (p.preload = () => {
+          // Any preloading
+        }),
+          (p.setup = () => {
+            p.createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+            p.colorMode(p.HSL, 360, 100, 100);
+            p.ellipseMode(p.RADIUS);
+          });
+
+        p.draw = () => {
+//            Draw all the colors
+          
+        };
+
+       
+        p.mouseClicked = () => {
+          // Mouse interaction
+        };
+      };
+
+      // Create P5
+      const CANVAS_EL = this.$refs.canvasHolder;
+      CANVAS_EL.style.width = CANVAS_WIDTH + "px";
+      CANVAS_EL.style.height = CANVAS_HEIGHT + "px";
+      new p5(s, CANVAS_EL);
+    },
 
 		data() {
       return {
