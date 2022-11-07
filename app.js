91fc8b8c262dacc78222ef500b9d23b9b0a467f2
Glitch (glitch-hello-website) <none>
2022-11-07 01:49:20 +0000

    📙🎐 Checkpoint ./app.js:270926/2921

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
-$.getJSON("xkcd.json", (data) => {
+// async function fetchPosts() {
+//   const response = await fetch('xkcd.json',  {
+//     method: 'get',
+//     mode: 'no-cors',
+//     headers: {
+//        'Access-Control-Allow-Origin' : '*'
+//     }})
+//   console.log(response)
+//   const posts = await response.json()
+//   return posts
+// }
+
+// fetchPosts()
+
+let url = 'xkcd.json'
+let params = {
+    method: 'get',
+    mode: 'no-cors',
+    headers: {
+       'Access-Control-Allow-Origin' : '*'
+    }}
+
+fetch(url, params)
+.then(response => {
+  console.log("got ", response)
+  return response.text()
+})
+.then((data) => {
   console.log(data)
-}).catch("err")
+  // resolve(data ? JSON.parse(data) : {})
+})
+.catch((error) => {
+  console.log("error")
+  // reject(error)
+})
